bcb361514fd8e4110e8fed082bb0906cbf249720
Quin Irving <qirvingbusiness@gmail.com>
2025-09-04 11:56:48 -0400

    Got the general message pump up and running to at least keep the screen up and able to process clicks such as closing the window.

--- SoftwareRasterizer3D/WinMain.cpp
+++ SoftwareRasterizer3D/WinMain.cpp
@@ -1,10 +1,23 @@
 #include <windows.h>
+#include <new>
+
+struct StateInfo {
+	int test = 42;
+};
+
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+const wchar_t WND_TITLE[] = L"3DGameEngine";
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow) {
 	// Register the window class
 	const wchar_t CLASS_NAME[] = L"Test Class";
+	StateInfo* pState = new (std::nothrow) StateInfo;
+
+	if (pState == NULL) {
+		return 0;
+	}
 
 	WNDCLASSEX wc{};
 	wc.cbSize = sizeof(WNDCLASSEX);
@@ -26,8 +39,6 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
 	
 	RegisterClassEx(&wc); // REgister the window with the operating system.
 
-	const wchar_t WND_TITLE[] = L"3DGameEngine";
-
 	// Create a new instance of the window.
 	HWND hwnd = CreateWindowEx(
 		0, CLASS_NAME, WND_TITLE,
@@ -35,7 +46,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
 
 		// Size & position
 		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
-		NULL, NULL, hInstance, NULL
+		NULL, NULL, hInstance, pState
 	);
 
 	if (hwnd == NULL) {
@@ -44,9 +55,80 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
 
 	ShowWindow(hwnd, nCmdShow);
 
-	//while (true);
+	MSG msg = { };
+	bool isRunning = true;
+
+	while (isRunning) {
+		while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+			if (msg.message == WM_QUIT) {
+				isRunning = false;
+				break;
+			}
+
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+
+		if (!isRunning) {
+			break;
+		}
+
+		// game logic Update()
+		// draw frame Render()
+	}
 }
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+	// additional data for message is contained w/in wParam & lParam look up each message
+	// as each is unique
+	StateInfo* pState;
+	if (uMsg == WM_CREATE) {
+		CREATESTRUCT* pCreate = reinterpret_cast<CREATESTRUCT*>(lParam);
+		pState = reinterpret_cast<StateInfo*>(pCreate->lpCreateParams);
+		SetWindowLongPtr(hwnd, GWLP_USERDATA, (LONG_PTR) pState);
+
+		/*
+		Can now get StateInfo pointer from GetWIndowLongPtr
+		LONG_PTR ptr = GetWindowLongPtr(hwnd, GWLP_USERDATA);
+		StateInfo *pState = reinterpret_cast<StateInfo*>(ptr);
+
+		could then create a small helper function:
+		inline StateInfo* GetAppState(HWND hwnd)
+		{
+			LONG_PTR ptr = GetWindowLongPtr(hwnd, GWLP_USERDATA);
+			StateInfo *pState = reinterpret_cast<StateInfo*>(ptr);
+			return pState;
+		}
+		*/
+	}
+	else {
+		pState = GetAppState(hwnd);
+	}
+
+	switch (uMsg) {
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	default:
+		break;
+
+	// can make separate functions handle specific messages to reduce code in one function.
+
+	// if some lengthy processing needs to occur inside window procedure, it will block
+	// all other messages so ensure to use something like:
+	/* 
+		Creating a new thread
+		Using a thread pool
+		Using asynchronous I/O calls
+		Using asynchronous procedure calls.
+	*/
+	}
+
 	return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
+
+inline StateInfo* GetAppState(HWND hwnd) {
+	LONG_PTR ptr = GetWindowLongPtr(hwnd, GWLP_USERDATA);
+	StateInfo* pState = reinterpret_cast<StateInfo*>(ptr);
+	return pState;
 }
