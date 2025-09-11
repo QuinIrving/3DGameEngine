ce32545b0635aa860e81d1685ea3145013827e46
Quin Irving <qirvingbusiness@gmail.com>
2025-09-11 18:35:22 -0400

    Some more folder structure changes.

--- SoftwareRasterizer3D/include/Models/Triangle.h
+++ SoftwareRasterizer3D/include/Models/Triangle.h
@@ -1,1 +1,16 @@
 #pragma once
+#include "Math/Vec3.h"
+#include "Vertex.h"
+
+class Triangle {
+};
+
+/*
+Vec3<T> GetNormalVec(){
+	const T u = pB - pA;
+	const T v = pC - pA;
+
+	return Vec3<T>::CrossProduct(u, v);
+}
+
+*/
